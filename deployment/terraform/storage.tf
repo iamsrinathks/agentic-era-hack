f680d9601a56d26dc0f0c700126a18c91b34947a
Alakh Mehta <alakh.mehta@publicissapient.com>
2025-09-18 21:35:33 +0530

    Completed-CI-CD

--- deployment/terraform/storage.tf
+++ deployment/terraform/storage.tf
@@ -28,7 +28,7 @@ resource "google_storage_bucket" "bucket_load_test_results" {
 
 resource "google_storage_bucket" "logs_data_bucket" {
   for_each                    = toset(local.all_project_ids)
-  name                        = "${each.value}-build-logs"
+  name                        = "${each.value}-csp-curator-logs"
   location                    = var.region
   project                     = each.value
   uniform_bucket_level_access = true
